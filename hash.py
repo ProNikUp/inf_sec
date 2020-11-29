79492db2fec7ced4296e24ae5a026347b2c64be4
ProNikUp <filippovnikita4@gmail.com>
2020-11-29 23:01:46 +0300

    hash

---
+++ hash.py
@@ -0,0 +1,15 @@
+import math
+
+def hash(n):
+    for i in range(len(n)):
+        p1 = (n[i]+n[i])*n[i]
+        p2 = (n[i]+n[i])*n[i]*p1
+        p3 = (n[i-1]+n[i-1])*n[i-1]*p2*p1
+        F = (p1+p2)*p3/((n[i-1]+n[i-1])*n[i-1]*p2*p1)
+        F1 = p1*p2+p3+F
+        F2 = math.log(F)**math.log(math.pi)
+        F3 = math.exp(F2)
+        F4 = F3 * F
+    return F4  
+ 
+print(hash([8,6,7,4]))
